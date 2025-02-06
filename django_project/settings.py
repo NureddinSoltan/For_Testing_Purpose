a5c51c69b68fdaed340e1f94dfd571e393ff7393
NureddinSoltan <noorsoltan40@gmail.com>
2025-02-06 06:12:14 +0300

    run collect static + connect vite with django but still there's a problem

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Local
     'homepage.apps.HomepageConfig',
+    # 3rd
+    'django_vite',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +58,9 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        # 'DIRS': [BASE_DIR / "homepage/templates"],
         'DIRS': [BASE_DIR / "templates"],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +122,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / "webapp/dist" # Matches vite config!
+]
+DJANGO_VITE = {
+    "default": {
+        "dev_mode": DEBUG,
+    }
+}
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
